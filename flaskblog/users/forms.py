2e42ba51f07033712d2b71a8b1feefbd41f5ceb4
lufefe <jumbaayabonga@gmail.com>
2020-07-26 03:13:25 +0200

    country field added - 26/07

--- flaskblog/users/forms.py
+++ flaskblog/users/forms.py
@@ -1,14 +1,16 @@
+from flask_login import current_user
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from flask_login import current_user
+
 from flaskblog.models import User
 
 
 class RegistrationForm(FlaskForm): # for registraion form input 
 	username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
 	email = StringField('Email', validators=[DataRequired(), Email()])
+	country = SelectField('Select your country', validate_choice = True)
 	password = PasswordField('Password', validators=[DataRequired()])
 	confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
 	submit = SubmitField('Sign Up')
--- flaskblog/users/routes.py
+++ flaskblog/users/routes.py
@@ -1,3 +1,4 @@
+import requests
 from flask import render_template, url_for, flash, redirect, request, Blueprint
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -14,10 +15,17 @@ users = Blueprint('users', __name__)
 def register():
     if current_user.is_authenticated:
         return redirect(url_for('main.home'))
+
+    r = requests.get('https://restcountries.eu/rest/v2/region/africa')
+    afri = r.json()
+
     form = RegistrationForm()
+
+    form.country.choices = [(i['name'], i['name']) for i in afri]
+
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username = form.username.data, email = form.email.data, password = hashed_password)
+        user = User(username = form.username.data, email = form.email.data,country = form.country.data,  password = hashed_password)
         db.session.add(user)
         db.session.commit()
         flash('Your account has been created! You are now able to log in',
