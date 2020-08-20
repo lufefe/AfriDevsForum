a78d6da533be5d9783f589b5879c3b6d1104985d
lufefe <jumbaayabonga@gmail.com>
2020-08-21 00:12:47 +0200

    back to auth

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -7,7 +7,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')  #
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'SQLALCHEMY_DATABASE_URI')  # database location
-
+    FLASKY_COMMENTS_PER_PAGE = 3
     # for sending email for forgot password
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
